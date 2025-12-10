efd6c8564e17768b8b8a1af6aef8de8d8a7de9fe
Future-R <36127692+Future-R@users.noreply.github.com>
2025-12-10 16:12:05 +0800

    feat: Load configuration from external JS file
    Moves application configuration from `metadata.json` to an external `config.js` file. This allows for easier local development and overrides, particularly when using the `file://` protocol where fetching JSON directly can be problematic due to CORS. A fallback to built-in defaults is included if `config.js` fails to load.

---
+++ config.js
@@ -0,0 +1,100 @@
+// 配置文件
+// 修改这里的 JSON 内容即可配置工具箱
+// 这是一个 JS 文件，为了能在本地直接运行 (file:// 协议)，我们不能使用 .json 文件
+
+window.TOOLBOX_CONFIG = {
+  "name": "未来工具箱",
+  "description": "各种小工具。",
+  "tools": [
+    {
+      "id": "counter",
+      "name": "手动计数器",
+      "desc": "可能是打击感最强的计数器",
+      "path": "counter",
+      "icon": "mouse-pointer-2",
+      "type": "internal",
+      "labelClass": "bg-[#f05e1c]",
+      "iconColor": "text-white"
+    },
+    {
+      "id": "anime-namer",
+      "name": "二次元起名器",
+      "desc": "味儿超冲",
+      "path": "anime-namer",
+      "icon": "sparkles",
+      "type": "internal",
+      "labelClass": "bg-[#e03c8a]",
+      "iconColor": "text-white"
+    },
+    {
+      "id": "dice",
+      "name": "电子骰子",
+      "desc": "爱信不信",
+      "path": "dice",
+      "icon": "box",
+      "type": "internal",
+      "labelClass": "bg-[#6B64EF]",
+      "iconColor": "text-white"
+    },
+    {
+      "id": "text-map",
+      "name": "文字地图编辑器",
+      "desc": "如果你不知道有什么用，那它就没用",
+      "path": "text-map",
+      "icon": "map",
+      "type": "internal",
+      "labelClass": "bg-[#5f27cd]",
+      "iconColor": "text-white"
+    },
+    {
+      "id": "diff-checker",
+      "name": "文本差异对比",
+      "desc": "Diff Checker",
+      "path": "https://www.diffchecker.com/zh-Hans/",
+      "icon": "file-diff",
+      "type": "external",
+      "labelClass": "bg-[#ff9f43]",
+      "iconColor": "text-white"
+    },
+    {
+      "id": "sauce-nao",
+      "name": "以图搜图",
+      "desc": "SauceNAO",
+      "path": "https://saucenao.com/",
+      "icon": "image",
+      "type": "external",
+      "labelClass": "bg-[#54a0ff]",
+      "iconColor": "text-white"
+    },
+    {
+      "id": "regex-online",
+      "name": "正则表达式",
+      "desc": "Regex Online",
+      "path": "https://www.jyshare.com/front-end/854/",
+      "icon": "code",
+      "type": "external",
+      "labelClass": "bg-[#10ac84]",
+      "iconColor": "text-white"
+    },
+    {
+      "id": "msd-manual",
+      "name": "诊疗手册",
+      "desc": "治出问题别找我",
+      "path": "https://www.msdmanuals.cn/home/symptoms",
+      "icon": "stethoscope",
+      "type": "external",
+      "labelClass": "bg-[#ee5253]",
+      "iconColor": "text-white"
+    },
+    {
+      "id": "guji-search",
+      "name": "古籍搜索",
+      "desc": "识典古籍",
+      "path": "https://www.shidianguji.com/",
+      "icon": "scroll",
+      "type": "external",
+      "labelClass": "bg-[#e79460]",
+      "iconColor": "text-zinc-800"
+    }
+  ]
+};
